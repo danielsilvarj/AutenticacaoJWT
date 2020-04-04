74dd737c661a6d0d0b15e944e132f342eef5f9ec
danielsilvarj <daniel.cg.rio@gmail.com>
2020-04-03 23:23:09 -0300

    Adicionar arquivos de projeto.

---
+++ AutenticacaoJWT/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using AutenticacaoJWT.Models;
+using AutenticacaoJWT.Repositories;
+using AutenticacaoJWT.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AutenticacaoJWT.Controllers
+{
+
+    [Route("v1/account")]
+    public class HomeController: ControllerBase
+    {
+        [HttpPost]
+        [Route("logar")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody]User model)
+        {
+
+            var user = UserRepository.Get(model.UserName, model.Password);
+
+            if (user == null)
+                return NotFound(new { message = "Usuario ou senha invalido" });
+
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anonimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "Membro")]
+        public string Employee() => String.Format("Funcionario");
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "Lider")]
+        public string Manager() => String.Format("Gerente");
+    }
+}
---
+++ AutenticacaoJWT/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using AutenticacaoJWT.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AutenticacaoJWT.Repositories
+{
+    public class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            List<User> users = new List<User>();
+            users.Add(new User { Id = 1, UserName = "Daniel", Password = "Daniel", Role = "Lider" });
+            users.Add(new User { Id = 1, UserName = "Dan", Password = "Dan", Role = "Membro" });
+            return users.Where(x => x.UserName.ToLower() == username.ToLower() && x.Password == password).FirstOrDefault();
+        }
+    }
+}
